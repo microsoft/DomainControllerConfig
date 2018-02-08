6f4bcfeebb1bbb04608789fc701912a22032e69f
Michael Greene <michael.greene@microsoft.com>
2018-02-07 22:34:30 -0600

    mvp 1

--- DomainControllerConfig.ps1
+++ DomainControllerConfig.ps1
@@ -1,11 +1,44 @@
-﻿<#PSScriptInfo
+
+<#PSScriptInfo
 
 .VERSION 0.1.0.0
 
-.GUID edd05043-2acc-48fa-b5b3-dab574621ba1
+.GUID 9e615a6f-ada0-454d-a314-dc4f76bf0f89
 
 .AUTHOR Michael Greene
 
-.DESCRIPTION Demonstrates a minimally viable domain controller configuration script
+.COMPANYNAME Microsoft
+
+.COPYRIGHT 
+
+.TAGS DSCConfiguration
+
+.LICENSEURI 
+
+.PROJECTURI 
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
 
 #>
+
+#Requires -Module xActiveDirectory
+#Requires -Module xStorage
+
+<# 
+
+.DESCRIPTION 
+ Demonstrates a minimally viable domain controller configuration script 
+
+#> 
+Param()
+
+
